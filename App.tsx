5fbea51e5a070f92a8e7180f6dc04c9d21ae640f
muhammad Aijaz Abbasi <muhammadaijaz76@gmail.com>
2020-08-05 14:09:36 +0500

    cards creatd

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  let arr:Number[] = [1,2,3,4,5,6,7,8]
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+       
+         <FlatList
+         data={arr}
+         renderItem={({item})=>(
+           <TouchableOpacity style={styles.card}>
+           <Text>
+               hjhjhgjgh
+           </Text>
+         </TouchableOpacity>
+         )}
+         keyExtractor = {(item)=>JSON.stringify(item)}
+         />
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  card:{
+    borderColor: "#eee",
+    borderWidth: 1,
+    minHeight: 250,
+    width: 350,
+    backgroundColor: "#296",
+    marginTop: 20,
+    marginBottom: 20,
+
+
+  }
+})
+export default App
